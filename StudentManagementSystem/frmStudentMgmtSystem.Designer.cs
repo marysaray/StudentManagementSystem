43685ac849f794cd683976ff9f3b906611e038b5
Mary Saray <msaray.ms@outlook.com>
2021-06-12 10:08:27 -0700

    Added Interface for Student Management System.

---
+++ StudentManagementSystem/frmStudentMgmtSystem.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace StudentManagementSystem
+{
+    partial class frmStudentMgmtSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstStudents = new System.Windows.Forms.ListBox();
+            this.cmdAddStudent = new System.Windows.Forms.Button();
+            this.cmdUpdateStudent = new System.Windows.Forms.Button();
+            this.cmdDeleteStudent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstStudents
+            // 
+            this.lstStudents.FormattingEnabled = true;
+            this.lstStudents.ItemHeight = 25;
+            this.lstStudents.Location = new System.Drawing.Point(0, 0);
+            this.lstStudents.Name = "lstStudents";
+            this.lstStudents.Size = new System.Drawing.Size(273, 454);
+            this.lstStudents.TabIndex = 0;
+            // 
+            // cmdAddStudent
+            // 
+            this.cmdAddStudent.Location = new System.Drawing.Point(351, 12);
+            this.cmdAddStudent.Name = "cmdAddStudent";
+            this.cmdAddStudent.Size = new System.Drawing.Size(150, 102);
+            this.cmdAddStudent.TabIndex = 1;
+            this.cmdAddStudent.Text = "Add Student";
+            this.cmdAddStudent.UseVisualStyleBackColor = true;
+            this.cmdAddStudent.Click += new System.EventHandler(this.cmdAddStudent_Click);
+            // 
+            // cmdUpdateStudent
+            // 
+            this.cmdUpdateStudent.Location = new System.Drawing.Point(351, 176);
+            this.cmdUpdateStudent.Name = "cmdUpdateStudent";
+            this.cmdUpdateStudent.Size = new System.Drawing.Size(150, 98);
+            this.cmdUpdateStudent.TabIndex = 2;
+            this.cmdUpdateStudent.Text = "Update Student";
+            this.cmdUpdateStudent.UseVisualStyleBackColor = true;
+            this.cmdUpdateStudent.Click += new System.EventHandler(this.cmdUpdateStudent_Click);
+            // 
+            // cmdDeleteStudent
+            // 
+            this.cmdDeleteStudent.Location = new System.Drawing.Point(351, 333);
+            this.cmdDeleteStudent.Name = "cmdDeleteStudent";
+            this.cmdDeleteStudent.Size = new System.Drawing.Size(141, 96);
+            this.cmdDeleteStudent.TabIndex = 3;
+            this.cmdDeleteStudent.Text = "Delete Student";
+            this.cmdDeleteStudent.UseVisualStyleBackColor = true;
+            this.cmdDeleteStudent.Click += new System.EventHandler(this.cmdDeleteStudent_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmdDeleteStudent);
+            this.Controls.Add(this.cmdUpdateStudent);
+            this.Controls.Add(this.cmdAddStudent);
+            this.Controls.Add(this.lstStudents);
+            this.Name = "Form1";
+            this.Text = "Student Management System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstStudents;
+        private System.Windows.Forms.Button cmdAddStudent;
+        private System.Windows.Forms.Button cmdUpdateStudent;
+        private System.Windows.Forms.Button cmdDeleteStudent;
+    }
+}
+
---
+++ StudentManagementSystem/frmStudentMgmtSystem.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentManagementSystem
+{
+    public partial class frmStudentMgmtSystem : Form
+    {
+        public frmStudentMgmtSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region NOTES
+            //// instance of student object
+            //Student s = new Student(); // Instantiating
+            //s.StudentId = "156842358";
+            //string id = s.StudentId;
+            //s.FullName = "Jane Doe";
+
+            //string display = s.GetDisplayText("\n");
+            //MessageBox.Show(display);
+
+            //#region Object Initialization Syntax
+
+            //Student s2 = new Student()
+            //{
+            //    StudentId = "123456789",
+            //    DateOfBirth = new DateTime(1985, 1, 1),
+            //    Email = "someone@student.cptc.edu",
+            //    FullName = "Unknown Name"
+            //};
+            //string unknownData = s2.GetDisplayText("\n");
+            #endregion
+
+            // load all students from the database.
+            List<Student> allStudents = StudentDB.GetAllStudents();
+            // Populate them in the list box.
+            PopulateStudentList(allStudents);
+        }
+        #region Method To Populate Student List
+        /// <summary>
+        /// Populates list of Students from the database.
+        /// </summary>
+        /// <param name="students"></param>
+        private void PopulateStudentList(List<Student> students)
+        {
+            // loop and add all the students into the list box
+            foreach (Student s in students)
+            {
+                lstStudents.Items.Add(s);
+            }
+        }
+        #endregion
+
+        private void cmdAddStudent_Click(object sender, EventArgs e)
+        {
+            // TODO: Create a form to add a student to the database
+            // After student is added place them into the list box
+        }
+
+        private void cmdUpdateStudent_Click(object sender, EventArgs e)
+        {
+            // Ensure user has selected a student in the listbox.
+            // Open a form with the students data populated.
+            // Once student is updated refresh listbox.
+        }
+
+        private void cmdDeleteStudent_Click(object sender, EventArgs e)
+        {
+            // Ensure user has selected a student in the listbox.
+            // Ask user if the want to delete the current student
+            // if yes: remove them from database and listbox.
+        }
+    }
+}
