8e06d11533c735be3081bac766a1ebfb71eb44ca
Mary Saray <msaray.ms@outlook.com>
2021-06-11 21:04:18 -0700

    Add project files.

---
+++ StudentManagementSystem/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // instance of student object
+            Student s = new Student(); // Instantiating
+            s.StudentId = "156842358";
+            string id = s.StudentId;
+            s.FullName = "Jane Doe";
+
+            string display = s.GetDisplayText("\n");
+            MessageBox.Show(display);
+
+            #region Object Initialization Syntax
+
+            Student s2 = new Student()
+            {
+                StudentId = "123456789",
+                DateOfBirth = new DateTime(1985, 1, 1),
+                Email = "someone@student.cptc.edu",
+                FullName = "Unknown Name"
+            };
+            string unknownData = s2.GetDisplayText("\n");
+            #endregion
+        }
+    }
+}
---
+++ StudentManagementSystem/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    /// <summary>
+    /// Represents an Individual CPTC Student.
+    /// </summary>
+    class Student
+    {
+        #region Properties
+        
+        // Id, Name, Date of birth, email
+        // Properties
+        /// <summary>
+        /// The 9 digit Student Identification number.
+        /// </summary>
+        public string StudentId { get; set; }
+
+        /// <summary>
+        /// The full legal name of the student.
+        /// First and Last name.
+        /// </summary>
+        public string FullName { get; set; }
+        /// <summary>
+        /// Student's email.
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Student's Date of birth.
+        /// </summary>
+        public DateTime DateOfBirth { get; set; }
+        #endregion
+        #region Methods
+        /// <summary>
+        /// Gets a string representation of current student instance.
+        /// </summary>
+        /// <param name="separator">
+        /// A string to separate each piece of information
+        /// </param>  
+        /// <returns></returns>
+        public string GetDisplayText(string separator)
+        {
+            return $"{StudentId}{separator}{FullName}{separator}" +
+                $"{Email}{separator}{DateOfBirth}";
+        }
+        /// <summary>
+        /// Register a student for a specific course.
+        /// </summary>
+        /// <param name="courseCode">
+        /// Code for the course to register for.
+        /// </param> 
+        public void Register(string courseCode)
+        {
+            throw new NotImplementedException(); // method stub
+        }
+        #endregion
+    }
+}
