22314d619654682ea406cbb3dec1efdb3bba13c6
Mary Saray <msaray.ms@outlook.com>
2021-06-13 11:31:14 -0700

    Created GetConnection method.

---
+++ StudentManagementSystem/DBHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    static class DBHelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            // create connection
+            SqlConnection con =
+                    new SqlConnection("Data Source =localhost;Initial Catalog=SMS;Integrated Security=True;");
+
+            return con;
+        }
+    }
+}
